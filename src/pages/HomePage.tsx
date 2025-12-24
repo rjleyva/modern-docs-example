816ccf3141d1490166509ceff066b893302eff20
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-24 18:03:22 -0500

    feat(home-page): wrap HomePage in MainLayout component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,20 @@
 import type React from 'react'
 import { Helmet } from '@dr.pogodin/react-helmet'
 import { usePageTitle } from '@/hooks/usePageTitle'
+import MainLayout from '@/layouts/MainLayout'
 
 const HomePage = (): React.JSX.Element => {
   const pageTitle = usePageTitle('Home Page 🚀')
 
   return (
-    <section id="home-page">
-      <Helmet>
-        <title>{pageTitle}</title>
-      </Helmet>
-      Hero
-    </section>
+    <MainLayout>
+      <section id="home-page">
+        <Helmet>
+          <title>{pageTitle}</title>
+        </Helmet>
+        Hero
+      </section>
+    </MainLayout>
   )
 }
 
