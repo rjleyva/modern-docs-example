f61508cddc45d5ea9b77a0b64eaab3eb2848a267
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-24 17:53:49 -0500

    feat(home-page): integrate Helmet

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,18 @@
 import type React from 'react'
+import { Helmet } from '@dr.pogodin/react-helmet'
+import { usePageTitle } from '@/hooks/usePageTitle'
 
 const HomePage = (): React.JSX.Element => {
-  return <section id="home-page">Hero</section>
+  const pageTitle = usePageTitle('Home Page 🚀')
+
+  return (
+    <section id="home-page">
+      <Helmet>
+        <title>{pageTitle}</title>
+      </Helmet>
+      Hero
+    </section>
+  )
 }
 
 export default HomePage
