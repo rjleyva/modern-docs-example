aedbe4d30b0c1c523a4324c3a5fa4750882462db
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-24 19:00:24 -0500

    refactor(home-page): remove MainLayout wrapper

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,17 @@
 import type React from 'react'
 import { Helmet } from '@dr.pogodin/react-helmet'
 import { usePageTitle } from '@/hooks/usePageTitle'
-import MainLayout from '@/layouts/MainLayout'
 
 const HomePage = (): React.JSX.Element => {
   const pageTitle = usePageTitle('Home Page 🚀')
 
   return (
-    <MainLayout>
-      <section id="home-page">
-        <Helmet>
-          <title>{pageTitle}</title>
-        </Helmet>
-        Hero
-      </section>
-    </MainLayout>
+    <section id="home-page">
+      <Helmet>
+        <title>{pageTitle}</title>
+      </Helmet>
+      Hero
+    </section>
   )
 }
 
