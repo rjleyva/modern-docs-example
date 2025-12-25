4e6e27cfc3fbde5fd24be4f35c2e5ab4846dea57
RJ Leyva <244540676+rjleyva@users.noreply.github.com>
2025-12-25 03:53:56 -0500

    feat(main): import globals.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { HelmetProvider } from '@dr.pogodin/react-helmet'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import routes from './routes/routes'
+import './styles/globals.css'
 
 const router = createBrowserRouter(routes, {
   basename: '/'
